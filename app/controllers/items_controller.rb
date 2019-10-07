d49f2531a04a3fb8b3b18687fcf281a9a42156b1
bethurban <beth.urban@gmail.com>
2019-10-07 13:58:46 -0400

    Build Items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,39 @@
 class ItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_item, only: [:show, :update, :delete]
+
+  def index
+    @items = Item.all
+    json_response(@items)
+  end
+
+  def create
+    @item = Item.create!(item_params)
+    json_response(@item,:created)
+  end
+
+  def show
+    json_response(@item)
+  end
+
+  def update
+    @item.update(item_params)
+    head :no_content
+  end
+
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :amount, :section)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
